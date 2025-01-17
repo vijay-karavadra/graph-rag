cce710fdafc73d4365b470d71eab0f254efd1aa7
Eric Pinzur <2641606+epinzur@users.noreply.github.com>
2025-01-17 11:53:59 +0100

    simplified edge class (#19)

--- src/graph_pancake/retrievers/edge.py
+++ src/graph_pancake/retrievers/edge.py
@@ -1,31 +1,6 @@
-from typing import Any
+from typing import NamedTuple, Any
 
-
-class Edge:
+class Edge(NamedTuple):
     """Represents an edge to all nodes with the given key/value incoming."""
-
     key: str
     value: Any
-
-    def __init__(self, key: str, value: Any) -> None:
-        self.key = key
-        self.value = value
-
-    def __str__(self) -> str:
-        return f"Edge({self.key}->{self.value}"
-
-    def __repr__(self) -> str:
-        return f"Edge(key={self.key}, value={self.value}"
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Edge):
-            return NotImplemented
-        return self.key == other.key and self.value == other.value
-
-    def __lt__(self, other: object) -> bool:
-        if not isinstance(other, Edge):
-            return NotImplemented
-        return (self.__str__()) < (other.__str__())
-
-    def __hash__(self) -> int:
-        return hash((self.key, self.value))
