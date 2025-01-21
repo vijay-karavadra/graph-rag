b27e6b3816ac86e9fa2eee803a784a133b45d56a
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-01-21 11:34:56 -0800

    add prints to drop astra keyspace

--- scripts/drop-astra-keyspace.py
+++ scripts/drop-astra-keyspace.py
@@ -8,5 +8,10 @@ keyspace = os.environ.get("ASTRA_DB_KEYSPACE", "default_keyspace")
 api_endpoint = os.environ["ASTRA_DB_API_ENDPOINT"]
 
 admin = AstraDBDatabaseAdmin(api_endpoint=api_endpoint, token=token)
-if keyspace in admin.list_keyspaces():
+keyspaces = admin.list_keyspaces()
+if keyspace in keyspaces:
+    print(f"Dropping keyspace '{keyspace}'")  # noqa: T201
     admin.drop_keyspace(keyspace)
+    print(f"Dropped keyspace '{keyspace}") # noqa: T201
+else:
+    print(f"Not dropping keyspace '{keyspace}'. Not in {keyspaces}") # noqa: T201
