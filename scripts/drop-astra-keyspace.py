e13816f0cd0725ee8201dd35289bf67543da9bcb
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-01-21 11:36:42 -0800

    add more logging

--- scripts/drop-astra-keyspace.py
+++ scripts/drop-astra-keyspace.py
@@ -11,7 +11,7 @@ admin = AstraDBDatabaseAdmin(api_endpoint=api_endpoint, token=token)
 keyspaces = admin.list_keyspaces()
 if keyspace in keyspaces:
     print(f"Dropping keyspace '{keyspace}'")  # noqa: T201
-    admin.drop_keyspace(keyspace)
-    print(f"Dropped keyspace '{keyspace}") # noqa: T201
+    result = admin.drop_keyspace(keyspace)
+    print(f"Dropped keyspace '{keyspace}: {result}") # noqa: T201
 else:
     print(f"Not dropping keyspace '{keyspace}'. Not in {keyspaces}") # noqa: T201
