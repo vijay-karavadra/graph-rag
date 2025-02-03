8b5d87ea468642bda97f547bfcc8f91ee1b54bb6
Eric Pinzur <2641606+epinzur@users.noreply.github.com>
2025-02-03 20:05:27 +0100

    added animals dataset to helpers (#99)
    * added animals dataset to helpers
    
    * lint
    
    * make it a function
    
    * fix linting
    
    ---------
    
    Co-authored-by: Ben Chambers <35960+bjchambers@users.noreply.github.com>

---
+++ packages/graph-rag-example-helpers/src/graph_rag_example_helpers/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .animals import fetch_animal_documents
+
+__all__ = [
+    "fetch_animal_documents",
+]
---
+++ packages/graph-rag-example-helpers/src/graph_rag_example_helpers/datasets/animals.py
@@ -0,0 +1,31 @@
+import json
+
+import requests
+from langchain_core.documents import Document
+
+ANIMALS_JSONL_URL = "https://raw.githubusercontent.com/datastax/graph-rag/refs/heads/main/data/animals.jsonl"
+
+
+def fetch_animal_documents() -> list[Document]:
+    """
+    Download and parse a list of Documents for use with Graph Retriever.
+
+    This is a small example dataset with useful links.
+
+    This method downloads the dataset each time -- generally it is preferable
+    to invoke this only once and store the documents in memory or a vector
+    store.
+
+    Returns
+    -------
+    list[Document]
+        The fetched animal documetns.
+    """
+    response = requests.get(ANIMALS_JSONL_URL)
+    response.raise_for_status()  # Ensure we got a valid response
+
+    return [
+        Document(id=data["id"], page_content=data["text"], metadata=data["metadata"])
+        for line in response.text.splitlines()
+        if (data := json.loads(line))
+    ]
