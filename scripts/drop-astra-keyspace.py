f284671aa0eee640948dc03a89bcf5b5febf7055
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2025-02-11 17:42:31 -0800

    don't drop default keyspace (#143)
    * don't drop default keyspace
    
    * lint

--- scripts/drop-astra-keyspace.py
+++ scripts/drop-astra-keyspace.py
@@ -1,10 +1,15 @@
 import os
+import sys
 
 from astrapy import AstraDBDatabaseAdmin
 from astrapy.authentication import StaticTokenProvider
 
 token = StaticTokenProvider(os.environ["ASTRA_DB_APPLICATION_TOKEN"])
-keyspace = os.environ.get("ASTRA_DB_KEYSPACE", "default_keyspace")
+keyspace = os.environ.get("ASTRA_DB_KEYSPACE")
+if keyspace is None:
+    print("No keyspace to drop.")  # noqa: T201
+    sys.exit()
+
 api_endpoint = os.environ["ASTRA_DB_API_ENDPOINT"]
 
 admin = AstraDBDatabaseAdmin(api_endpoint=api_endpoint, token=token)
